8193d33ef0b34b5c16cdeba69eabd715c83eed1e
NsdHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2022-09-05 07:54:53 +0300

    feat: added truck route

---
+++ apps/driver/src/app/truck/truck.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fleet-driver-truck',
+  templateUrl: './truck.component.html',
+  styleUrls: ['./truck.component.scss'],
+})
+export class TruckComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
